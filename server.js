17813e359df66422add7b625bb2c14122683db28
ukhan57 <ukhan57@myseneca.ca>
2022-09-28 20:25:35 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var HTTP_PORT = process.env.port || 8080;
+
+function onHttpSStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+//setting up a initial router function to listen on '/'
+app.get("/", function(req,res){
+res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+//setting up a router function to listen on './blog'
+app.get("/blog", function(req,res){
+res.sendFile(path.join(__dirname, "/blog.html"));
+});
+
+app.listen(HTTP_PORT, onHttpSStart);
